2dc603779c49124b39793588ff87726a2a103877
Xiaoyi Liu <lxy878@gmail.com>
2020-11-14 16:52:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux'
 
-const store = createStore(shoppingListItemReducer)
+const store = createStore(
+    shoppingListItemReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
